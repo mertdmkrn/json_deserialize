5efd921b13cca4e2c5acd2fccf2030b1d04e5acd
Mert Demirkıran <mertdmkrn37@gmail.com>
2020-07-20 01:52:46 +0300

    First Commit

---
+++ json-deserialize/src/json_deserialize/model/Employee.java
@@ -0,0 +1,42 @@
+package json_deserialize.model;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String empDept;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+	public String getEmpAd() {
+		return empName;
+	}
+	
+	public void setEmpAd(String empName) {
+		this.empName = empName;
+	}
+	
+	public String getEmpDept() {
+		return empDept;
+	}
+	
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [Id = " + empId + ", empName = " + empName + ", empDept = " + empDept + "]";
+	}
+	
+	
+	
+	
+	
+}
